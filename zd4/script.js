0130c3455e45ae98423d3e42fe6896927519287b
Dim <lifanchuk.d@gmail.com>
2018-04-26 10:50:40 +0300

    lb5

---
+++ zd4/script.js
@@ -0,0 +1,31 @@
+function getNthItem(a, n) {
+	var h = 0;
+	for (i = 0; i < n; i++){
+		f = a - 2 * h
+		h = f;
+	}
+	return h;
+}
+
+function assert(expression, message) {
+	if(expression) {
+		console.log("+", message);
+	}
+	else {
+		console.error("-", message);
+	}
+}
+
+function runTask4() {
+	assert(getNthItem(5, 1) ==  5, "Case '#0'");
+	assert(getNthItem(11, 0) ==  0, "Case '#1'");
+	assert(getNthItem(10, 3) ==  30, "Case '#2'");
+	assert(getNthItem(0, 1) ==  0, "Case '#3'");
+	assert(getNthItem(5, -1) ==  0, "Case '#4'");
+	assert(getNthItem(30, 2) ==  -30, "Case '#5'");
+	assert(getNthItem(-5, 40) ==  1832519379625, "Case '#6'");
+	assert(getNthItem(12, 8) ==  -1020, "Case '#7'");
+	console.log("Done!");
+}
+
+runTask4();
---
+++ zd5/script.js
@@ -0,0 +1,32 @@
+function sumDigits(n) {
+	var sum = 0;
+    while (n > 0)
+		{
+		 sum += n % 10
+		 n = Math.floor(n/10)
+		}
+    return sum;
+}
+
+function assert(expression, message) {
+	if(expression) {
+		console.log("+", message);
+	}
+	else {
+		console.error("-", message);
+	}
+}
+
+function runTask5() {
+	assert(sumDigits(717) == 15, "Case '717'");
+	assert(sumDigits(115) == 7, "Case '115'");
+	assert(sumDigits(918) == 18, "Case '918'");
+	assert(sumDigits(5) == 5, "Case '5'");
+	assert(sumDigits(52) == 7, "Case '52'");
+	assert(sumDigits(0) == 0, "Case '0'");
+	assert(sumDigits(103) == 4, "Case '103'");
+	assert(sumDigits(188) == 17, "Case '188'");
+	console.log("Done!");
+}
+
+runTask5();
